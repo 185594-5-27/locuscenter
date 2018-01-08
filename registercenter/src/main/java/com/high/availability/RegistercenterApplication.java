968a7dfa57b9aa694f86e0921cdf6f279fcc7b91
林泽锋 <282245889@qq.com>
2018-01-08 20:02:26 +0800

    Initial commit

---
+++ registercenter/src/main/java/com/high/availability/RegistercenterApplication.java
@@ -0,0 +1,14 @@
+package com.high.availability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistercenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistercenterApplication.class, args);
+	}
+}
