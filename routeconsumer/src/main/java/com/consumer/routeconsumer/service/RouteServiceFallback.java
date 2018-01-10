cad0177dd416c7d55948fab71d983c647071e416
282245889@qq.com <575088Wuling>
2018-01-10 22:05:52 +0800

    修改消费者没有被zipkin获取的Bug

--- routeconsumer/src/main/java/com/consumer/routeconsumer/service/RouteServiceFallback.java
+++ routeconsumer/src/main/java/com/consumer/routeconsumer/service/RouteServiceFallback.java
@@ -1,7 +1,9 @@
 package com.consumer.routeconsumer.service;
 
 import com.consumer.routeconsumer.entity.Trajectory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RouteServiceFallback implements RouteService {
 
     @Override
